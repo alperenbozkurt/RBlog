4e79e89787c1d2210c958193d8d52ac3d2083c07
Alperen Bozkurt <bozkurttalperen@gmail.com>
2016-10-27 18:38:00 +0300

    git control

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+# Git Control
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
